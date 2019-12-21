6c8d6d0f980ed4771f83bc0844f6b576afd0976e
Yimeng Yu <yimeng.yu@gmail.com>
2019-12-20 22:14:33 -0800

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
