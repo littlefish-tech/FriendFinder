df13c7a2bcec70692d84cbfa21f88f8145707914
Yimeng Yu <yimeng.yu@gmail.com>
2019-12-30 21:47:00 -0800

    update html and friend list

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+//==========================================================
+//DATA
+// Below data will hold all of the friends list.
+//==========================================================
+
+var friendsArr = [
+    {
+        friendsFirstName: "John Smith",
+        friendsPhoto: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi1gJbmk9_mAhUSMH0KHXTgCbEQjRx6BAgBEAQ&url=https%3A%2F%2F99designs.com%2Fother-design%2Fcontests%2Fdesign-sweet-panda-cartoon-character-erstelle-einen-s%25C3%25BC%25C3%259Fen-865362&psig=AOvVaw3t8v-RVq2Qe2y_Fsv9K0N1&ust=1577856400021647",
+        friendsScoreArr: [
+            "3",
+            "1",
+            "4",
+            "2",
+            "5",
+            "5",
+            "3",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        friendsFirstName: "Peter Liu",
+        friendsPhoto: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj85cTtlN_mAhVKrp4KHao6A2AQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_30015171_stock-vector-cute-female-tiger-cartoon.html&psig=AOvVaw3Cckkx8WnuXiR-XTLCHhjV&ust=1577856821816912",
+        friendsScoreArr: [
+            "2",
+            "4",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "4",
+            "2",
+            "3"
+        ]
+    },
+    {
+        friendsFirstName: "Sam Summer",
+        friendsPhoto: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjXwq-yld_mAhUQsJ4KHWThBAIQjRx6BAgBEAQ&url=https%3A%2F%2Ffortune.com%2F2019%2F08%2F06%2Fviacom-buys-garfield-nickelodeon%2F&psig=AOvVaw075PKA18zMFHx5mX1dQBFy&ust=1577856945290173",
+        friendsScoreArr: [
+            "1",
+            "5",
+            "4",
+            "4",
+            "3",
+            "3",
+            "4",
+            "2",
+            "1",
+            "5"
+        ]
+    },
+];
+
+//export the Array;
+//This makes it accessable to other files useing require.
+
+module.exports = friendsArr;
--- server.js
+++ server.js
@@ -8,3 +8,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//ROUTER
+//The below points our server to a series of "route" files.
+//These routes give our server a "map" of 
