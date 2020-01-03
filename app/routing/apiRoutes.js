2afdc993d56cee58c9ec1581159b7352ba6b5221
Yimeng Yu <yimeng.yu@gmail.com>
2020-01-02 23:44:57 -0800

    fixed arr num string to arr num bug

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,15 +3,14 @@
 //We are linking our routes to a series of "data" sources.
 //These data sources hold arrays of information on friends-data
 //=============================================================
-
 var friendsData = require ("../data/friends");
+//console.log(friendsData);
 
 //==============================================================
 //
 //ROUTING
 //
 //==============================================================
-
 module.exports = function(app) {
     // API GET Requests
     //Below code handles when users "visit" a page,
@@ -21,7 +20,6 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res){
         res.json(friendsData);
     });
-
     //API POST Requests
     //Below code handles when a user submits form data ( a JSON object)
     //... the JSON is pushed to the appropriate Javascript array
@@ -35,36 +33,38 @@ module.exports = function(app) {
         // req.body is available since we're using the body parsing middleware
         
         friendsData.push(req.body);
-        res.json(true);
-        
+        res.json(true);      
+    
+        scoreDiffArr = [];
+   // app.get("/api/match", function(req, res){
+        for (var i = 0; i < friendsData.length-1; i++){
+          //  console.log(friendsData)
+           var friDataScore = friendsData[i].friendsScoreArr
+           console.log(friDataScore)
+           var scoreNumArr = friDataScore.map(Number);
+           //console.log(scoreNumArr);
+           var surveyNumArr = req.body.friendsScoreArr.map(Number);
+           console.log(surveyNumArr)
 
-        for (var i = 0; i < friendsData.length; i++){
-            var scoreList =  parseInt(friendsData[i].friendsScoreArr)
-           
-            for (var j = 0; j < scoreList.length; j++){
-                newScoreList = parseInt(req.body.friendsScoreArr)
-                var currentScoreNum = newScoreList[j];
-                var friendsArrScoreNum = scoreList[j]
-                var qScoreDiff = Math.abs(currentScoreNum - friendsArrScoreNum);
-                qScoreDiff += qScoreDiff;
-                //console.log(qScoreDiff)
-
-                var mostMatchIndex = n;
-                for (var k = 0; k < friendsData[n].friendsScoreArr.length; k++){
-                    var mostMatchNum = parseInt(riendsData[n].friendsScoreArr);
-                    var currentDiff = Math.abs(currentScoreNum - mostMatchNum[k]);
-
-                    currentDiff += currentDiff;
-                    if (qScoreDiff = currentDiff){
-                        res.json(friendsData[mostMatchIndex])
-                    }
-
-                }
-
-
-          }
+           var scoreDiffSum = 0;
+            for (var j = 0; j < 10; j++){
+                
+                var qScoreDiff = Math.abs(scoreNumArr[j] - surveyNumArr[j]);
+                scoreDiffSum += qScoreDiff;
             }
+            scoreDiffArr.push(scoreDiffSum);
+                
+        }
+        console.log(scoreDiffArr)
+                //console.log(qScoreDiff)
+                var minQS = Math.min(scoreDiffArr);
+
+                var mostMatchIndex = scoreDiffArr.indexOf(minQS);
+                console.log(mostMatchIndex)
+                res.json(friendsData[mostMatchIndex]);                    
+       
         });
+   //     });
     }
 
 
